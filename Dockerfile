f5f807f5725ed5af27e137e3decfeac8eed34a4a
the-lundy <lundy_T@msn.com>
2022-02-13 20:14:03 -0700

    Up to Postgres Setup halfway

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+#pull official base image
+
+FROM python:3.10.1-slim-buster
+
+# set working directory
+
+# set working directory
+RUN mkdir -p /usr/src/app
+
+WORKDIR /usr/src/app
+
+#set environment variables
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+#add and install requirements
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+
+#add app
+COPY . .
+
+#run server
+
+CMD python manage.py run -h 0.0.0.0
+
--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
 
-
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
+# import sys
 
 #instantiate the app
 app = Flask(__name__)
@@ -11,7 +13,28 @@ api = Api(app)
 #API = application programming interface
 
 #Sets config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+#instantiate the db
+db = SQLAlchemy(app)
+
+
+#If wanting to view config that was loaded
+#need to 'import sys'
+# print(app.config, file=sys.stderr)
+
+#model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128),nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(),default=True, nullable=False)
+
+    def __init__(self,username,email):
+        self.username = username
+        self.email = email
 
 
 
